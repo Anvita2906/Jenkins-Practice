20c0067dc86b8fe1d894dd040574a10c4ea8ee80
Anvita2906 <57917411+Anvita2906@users.noreply.github.com>
2024-06-07 04:31:33 +0530

    testing 4

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 print("This repository is for practicing Jenkins")
 print("This print is for checking if jenkins is able to pull in the latest changes from git")
-print("This is to check the poll SCM feature of build trigger")
+print"This is to check the poll SCM feature of build trigger")
